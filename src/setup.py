ca2ca7910490b630caeff37d227581b0387d6e4c
Anton Homilius <antonhomilius@Antons-MacBook-Air.local>
2022-12-02 10:53:10 +0100

    test

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(name='Approx_matching', ext_modules=cythonize('/home/runner/work/project-5-python-asg/project-5-python-asg/src/Approx_Positions_Cython.pyx'))
+setup(name='Approx_matching', ext_modules=cythonize('Approx_Positions_Cython.pyx'))
